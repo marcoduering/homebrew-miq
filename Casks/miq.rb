0a8b122be6abb6fca3a920bd2873b7c38abffba2
Marco Duering <githubmd@marco-duering.net>
2026-05-23 21:20:34 +0200

    v0.5.0

--- Casks/miq.rb
+++ Casks/miq.rb
@@ -1,6 +1,6 @@
 cask "miq" do
-  version "0.4.2"
-  sha256 "fd0e6a220bbd8fcf2298bf5f122b9a6089feb4dd4c88f8d24bab8116c7c3b753"
+  version "0.5.0"
+  sha256 "80f7793c7d44d0f4076eff4c44f7577bd701515e455b6a23f0bf0a840db4667b"
 
   url "https://github.com/marcoduering/MIQ/releases/download/v#{version}/MIQ.app.zip"
   name "MIQ"
